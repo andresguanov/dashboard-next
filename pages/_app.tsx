2bbd82c047afbb7694777bd446a8eaa994390c54
andresguanov <andresguanov@gmail.com>
2022-02-20 15:03:00 -0500

    Create a MainLayout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import MainLayout from '@components/MainLayout'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <MainLayout>
+    <Component {...pageProps} />
+  </MainLayout>
+)
 
 export default MyApp
